5b672da7c5f266053d59b5db70caae97ce5d2ec1
JoakBouy <B.joak@alustudent.com>
2023-05-24 14:19:10 +0300

    tests review is a subclass of basemodel

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Test suite for Review class in models.review"""
+import unittest
+
+from models.base_model import BaseModel
+from models.review import Review
+
+
+class TestReview(unittest.TestCase):
+    """Test cases for the Review class"""
+
+    def setUp(self):
+        self.review = Review()
+        self.attr_list = [
+            "place_id",
+            "user_id",
+            "text"
+        ]
+
+    def test_review_is_a_subclass_of_basemodel(self):
+        self.assertTrue(issubclass(type(self.review), BaseModel))
