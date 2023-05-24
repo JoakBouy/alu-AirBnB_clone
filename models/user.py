bde77d9a65f6c5a0316103fcbf05b9278e0583c4
JoakBouy <B.joak@alustudent.com>
2023-05-24 16:03:24 +0300

    updates to user class

--- models/user.py
+++ models/user.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Class User that inherits from the BaseModel class"""
-
-from models import BaseModel
+"""Class representing a user"""
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class"""
-
+    """Details of a user"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    """def __init__(self, *args, **kwargs):
+        Initialize a User instance
+        super().__init__(*args, **kwargs)"""
