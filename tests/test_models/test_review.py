488b7cc1579dbc155b7f47eafac967fd101b07d8
JoakBouy <B.joak@alustudent.com>
2023-05-24 14:19:56 +0300

    Test cases for the Review class

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -19,3 +19,12 @@ class TestReview(unittest.TestCase):
 
     def test_review_is_a_subclass_of_basemodel(self):
         self.assertTrue(issubclass(type(self.review), BaseModel))
+
+    def test_attrs_are_class_attrs(self):
+        for attr in self.attr_list:
+            self.assertTrue(hasattr(self.review, attr))
+
+    def test_class_attrs(self):
+        for attr in self.attr_list:
+            self.assertIs(type(getattr(self.review, attr)), str)
+            self.assertFalse(bool(getattr(self.review, attr)))
