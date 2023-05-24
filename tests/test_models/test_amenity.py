d32c9917dda6972ab5f0fb27a0272d66cb583363
JoakBouy <B.joak@alustudent.com>
2023-05-24 14:11:41 +0300

    tests amenity is a subclass of basemodel

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Test suite for Amenity class of the models.amenity module"""
+import unittest
+
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """Test cases for the Amenity class"""
+
+    def setUp(self):
+        self.amenity = Amenity()
+
+    def test_amenity_is_a_subclass_of_basemodel(self):
+        self.assertTrue(issubclass(type(self.amenity), BaseModel))
+
